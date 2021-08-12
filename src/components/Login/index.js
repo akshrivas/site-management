f4da02af8e4a2bf65377daf3fbc00d6602734a12
Ayush Gupta <ayush.gupta.1197@gmail.com>
2021-08-12 12:21:56 +0530

    fix: added formik validations

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,1 +1,134 @@
-export { default } from './Login';
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
+import { useAuth } from '../../context/AuthProvider';
+import * as yup from 'yup';
+import { useFormik } from 'formik';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import { useStyles } from './loginStyles';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+
+
+export default function Login() {
+  const classes = useStyles();
+  const validationSchema = yup.object({
+    email: yup
+      .string()
+      .email('Enter a valid email.')
+      .required('This field is required.'),
+    password: yup
+    .string()
+    .required('This field is required.')
+  });
+  const [user, setUser] = useState({
+    email: '',
+    password: ''
+  });
+  const handleSubmit = (values) => {
+    console.log(`submitted with ${values}`);
+  }
+  const formik = useFormik({
+    initialValues: { ...user },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+        handleSubmit(values);
+    },
+  });
+  return (
+    <>
+      <Container component="main" maxWidth="xs">
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <form className={classes.form} onSubmit={formik.handleSubmit}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              type="email"
+              // autoComplete="email"
+              autoFocus
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              error={formik.touched.email && formik.errors.email}
+              helperText={formik.touched.email && formik.errors.email}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              // autoComplete="current-password"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              error={formik.touched.password && formik.errors.password}
+              helperText={formik.touched.password && formik.errors.password}
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="/dashboard" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+        <Box mt={8}>
+          <Copyright />
+        </Box>
+      </Container>
+    </>
+  );
+}
