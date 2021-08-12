1d95311de1c787a6a3ddd679f8954b61ce4b28cd
Ayush Gupta <ayush.gupta.1197@gmail.com>
2021-08-12 12:05:09 +0530

    fix: added firebase config & auth provider

---
+++ src/config/firebase.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAwEuNdg5BHtsqUzJasLvZccoi0i0zd9BA",
+    authDomain: "homeo-88496.firebaseapp.com",
+    projectId: "homeo-88496",
+    storageBucket: "homeo-88496.appspot.com",
+    messagingSenderId: "195882754642",
+    appId: "1:195882754642:web:c392944e5255b36d96f58b",
+    measurementId: "G-0Y3F0W4N0K"
+};
+
+export default firebaseConfig;
