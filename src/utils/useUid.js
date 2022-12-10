0c975baa966932ee8a2bb71248796dddcf801d37
Ashish Kumar Shrivastav <ashish@Ashishs-MacBook-Pro.local>
2022-12-10 18:17:18 +0530

    fixed issues

--- src/utils/useUid.js
+++ src/utils/useUid.js
@@ -1,7 +1,7 @@
 import { useSelector } from 'react-redux';
 
 const useUid = () => {
-    const uid = useSelector((state) => state.firebase.auth.uid);
+    const uid = useSelector((state) => state.firebase.auth.uid) || '48X6bGqxqTVIDIoxD1l4006s8pr2';
     return uid;
   };
   
